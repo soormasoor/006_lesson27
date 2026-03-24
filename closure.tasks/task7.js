fa5eb3d4f963793d253d36ba5974139cfb30ea84
Suren Aslanyan <suren.aslanyan.am@gmail.com>
2026-03-24 20:48:38 +0400

    Added Closure Task 7

---
+++ closure.tasks/task7.js
@@ -0,0 +1,18 @@
+function createCalculator(operation) {
+  const operations = {
+    add: (a, b) => a + b,
+
+    substract: (a, b) => a - b,
+
+    multiply: (a, b) => a * b,
+
+    divide: (a, b) => a / b,
+  };
+
+  return operations[operation];
+}
+
+const add = createCalculator("add");
+console.log(add(3, 5)); // Output: 8
+const multiply = createCalculator("multiply");
+console.log(multiply(2, 4)); // Output: 8
